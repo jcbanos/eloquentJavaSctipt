7562e7e97639825928c923f128eb69994f2cb2f7
Juan Carlos Banos Mafud <juancarlosbanosmafud@gmail.com>
2020-07-18 12:44:46 -0500

    Finished flatten and loop.

---
+++ Chapter 5 exercises/loop.js
@@ -0,0 +1,14 @@
+// Your code here.
+
+function loop(value, test, update, body){
+  while(test(value)){
+    body(value);
+    value = update(value);
+  }
+  return value;
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
